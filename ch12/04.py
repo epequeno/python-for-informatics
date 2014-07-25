2bad7d105086a30f30081e083bf5617b1af2be30
Estevan Pequeno <estevan.pequeno@gmail.com>
2014-07-25 18:30:39 -0500

    variety is the spice of life

--- ch12/04.py
+++ ch12/04.py
@@ -15,14 +15,11 @@ Test your program on several small web pages as well as some larger web pages.
 import re
 import requests
 
-url = 'http://www.py4inf.com'
+url = raw_input("Enter url: ")
 req = requests.get(url)
 pattern = re.compile('<p>')
-content_lines = [line for line in req.iter_lines()]
+matches = [line for line in req.iter_lines()
+           if re.search(pattern, line)]
 
-count = 0
-for line in content_lines:
-    if re.search(pattern, line):
-        count += 1
 
-print "There are %d <p> tags @ %s" % (count, url)
+print "There are %d <p> tag(s) @ %s" % (len(matches), url)
